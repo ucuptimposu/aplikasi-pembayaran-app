61fc3dd34ae2e3f4b535a4afce1a4e4eed18d4bd
= <=>
2019-04-20 23:03:56 +0800

    update

--- app/src/main/java/com/timposu/droid/aplikasipenjualanapp/fragment/CekTagihanFragment.java
+++ app/src/main/java/com/timposu/droid/aplikasipenjualanapp/fragment/CekTagihanFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.timposu.droid.aplikasipenjualanapp.R;
 
@@ -14,6 +16,19 @@ public class CekTagihanFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fr_cek_tagihan, container, false);
+        View fragmentView = inflater.inflate(R.layout.fr_cek_tagihan, container, false);
+        Button button = (Button) fragmentView.findViewById(R.id.btnCekTagihan);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction =
+                        CekTagihanFragment.this.getActivity().getSupportFragmentManager().beginTransaction();
+
+                fragmentTransaction.replace(R.id.fragment_sebelum_login, new HasilTagihanFragment());
+                fragmentTransaction.commit();
+            }
+        });
+        return fragmentView;
     }
+
 }
--- app/src/main/java/com/timposu/droid/aplikasipenjualanapp/fragment/HasilTagihanFragment.java
+++ app/src/main/java/com/timposu/droid/aplikasipenjualanapp/fragment/HasilTagihanFragment.java
@@ -1,31 +1,35 @@
 package com.timposu.droid.aplikasipenjualanapp.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.timposu.droid.aplikasipenjualanapp.R;
-import com.timposu.droid.aplikasipenjualanapp.SetelahLoginActivity;
 
-public class LoginFragment extends Fragment {
+public class HasilTagihanFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View fragment = inflater.inflate(R.layout.fr_login, container, false);
-        Button btnLogin = (Button) fragment.findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        View fragmentView = inflater.inflate(R.layout.fr_hasil_tagihan, container, false);
+
+        Button button = (Button) fragmentView.findViewById(R.id.btnBack);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent setelahLoginActivity = new Intent(getContext(), SetelahLoginActivity.class);
-                startActivity(setelahLoginActivity);
+                FragmentTransaction ft = HasilTagihanFragment.this.getActivity().
+                        getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment_sebelum_login, new CekTagihanFragment());
+                ft.commit();
             }
         });
-        return fragment;
+
+        return fragmentView;
     }
 }
